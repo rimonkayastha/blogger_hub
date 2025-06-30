ed332fd89f9f313d845eab67465ffde32b8a3b3d
Rimon Kayastha <rimonkayastha@gmail.com>
2025-06-30 15:47:52 +0545

    feat: add login page

---
+++ blogger_hub_project/user/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import CustomUser
+
+class UserLoginForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
--- blogger_hub_project/user/views.py
+++ blogger_hub_project/user/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import CustomUser
+from .forms import UserLoginForm
 
 # Create your views here.
 def login_page(request):
     if request.user.is_authenticated:
         return redirect('main_home')
-    return render(request, 'templates/login.html', {})
+    if request.method == 'POST': 
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            if CustomUser.objects.filter(email=email, password=password).exists():
+                return redirect('main_home')
+            else:
+                messages.error(request, 'Invalid email or password')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
